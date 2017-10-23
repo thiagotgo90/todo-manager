7650fc21ad8a8a2d99e1bc035c212fe61625d230
thiagotgo90 <thiago.tgo90@gmail.com>
2017-10-23 18:51:35 -0200

    Todo com AWS

---
+++ src/main/java/org/tgo/todomanager/TodoController.java
@@ -0,0 +1,46 @@
+package org.tgo.todomanager;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import org.tgo.todomanager.model.Todo;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+@RestController
+@RequestMapping("/todo")
+public class TodoController {
+
+	private static final String BUCKET = "todo-attached-files";
+
+	@GetMapping
+	public Todo recuperar() {
+		return new Todo();
+	}
+
+	@RequestMapping(method = RequestMethod.POST, consumes = "multipart/form-data")
+	public ResponseEntity<Object> salvar(Todo todo, @RequestParam(name = "anexo", required = false) MultipartFile anexo)
+			throws IOException {
+
+		String attachment = UUID.randomUUID().toString();
+		AmazonS3 s3client = AmazonS3ClientBuilder.standard().build();
+
+		s3client.putObject(
+				new PutObjectRequest(BUCKET, attachment, anexo.getInputStream(), new ObjectMetadata()));
+
+		todo.setAttachment(attachment);
+
+		return ResponseEntity.accepted().body(todo);
+	}
+}
---
+++ src/main/java/org/tgo/todomanager/model/Todo.java
@@ -0,0 +1,36 @@
+package org.tgo.todomanager.model;
+
+public class Todo {
+
+	private String title;
+
+	private String description;
+
+	private String attachment;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAttachment() {
+		return attachment;
+	}
+
+	public void setAttachment(String attachment) {
+		this.attachment = attachment;
+	}
+
+
+}
